3bb1ec7c39b92db5c8a8699127773f0cbeb2eeef
AleksandrBara <alexbar130379@gmail.com>
2022-11-23 11:53:31 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -5,17 +5,27 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from collections import defaultdict
 from pprint import pprint
 from more_itertools import chunked
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 def on_reload():
     with open("books.json") as file:
         books = json.load(file)
-    hulf_of_books = len(books) // 2
-    chuncked_books = list(chunked(books, hulf_of_books))
-    template = env.get_template('template.html')
-    rendered_page = template.render(chuncked_books=chuncked_books)
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
-    print("page reloaded!")
+    html_pages_path = os.path.join(BASE_DIR, 'html_pages')
+    os.makedirs(html_pages_path, exist_ok=True)
+    number_of_books_per_page = 20
+    # hulf_of_books = len(books) // 2
+    chuncked_books = list(chunked(books, number_of_books_per_page))
+    for num, books in enumerate(chuncked_books):
+        template = env.get_template('template.html')
+        page_path = os.path.join(
+            html_pages_path,
+            'index{}.html'.format(num))
+        rendered_page = template.render(chuncked_books=chuncked_books)
+        with open(page_path, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
+        print("page reloaded!")
 
 
 if __name__ == '__main__':
