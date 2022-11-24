8965e6890fbb60e23fd5a8ce85d9bc4c28949e92
AleksandrBara <alexbar130379@gmail.com>
2022-11-24 15:53:01 +0300

    Add paginator.

--- render_website.py
+++ render_website.py
@@ -19,18 +19,18 @@ def on_reload():
     rows_per_page = 10
     number_of_pages = ceil(len(books) / number_of_books_per_page)
     chuncked_books = list(chunked(books, number_of_books_per_page))
+    last_page = len(chuncked_books) - 1
     for num, books in enumerate(chuncked_books):
         template = env.get_template('template.html')
         page_path = os.path.join(
             html_pages_path,
             'index{}.html'.format(num))
         chuncked_books = list(chunked(books, rows_per_page))
-        previous_page_num = num - 1
-        next_page_num = num + 1
         rendered_page = template.render(
             chuncked_books=chuncked_books,
             number_of_pages=number_of_pages,
-            current_page_number=num
+            current_page_number=num,
+            last_page=last_page
         )
         with open(page_path, 'w', encoding="utf8") as file:
             file.write(rendered_page)
